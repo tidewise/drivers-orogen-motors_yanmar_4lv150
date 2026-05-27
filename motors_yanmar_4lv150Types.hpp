c3a56911d5e306700cafd3bcdc044c1a9dfb20fe
seixasxbr <mateus.seixas@tidewise.io>
2026-05-27 15:32:21 -0300

    first commit

---
+++ motors_yanmar_4lv150Types.hpp
@@ -0,0 +1,14 @@
+#ifndef motors_yanmar_4lv150_TYPES_HPP
+#define motors_yanmar_4lv150_TYPES_HPP
+
+/* If you need to define types specific to your oroGen components, define them
+ * here. Required headers must be included explicitly
+ *
+ * However, it is common that you will only import types from your library, in
+ * which case you do not need this file
+ */
+
+namespace motors_yanmar_4lv150 {
+}
+
+#endif
